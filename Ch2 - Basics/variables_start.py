2d70d3cc3eb9fbab5de9697e16f7628cd810b353
Roger Perez <ropere@microsoft.com>
2024-03-02 20:32:44 -0800

    learn global

--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -5,33 +5,46 @@
 
 
 # Basic data types in Python: Numbers, Strings, Booleans, Sequences, Dictionaries
-myint = 5
-myfloat = 13.2
-mystr = "This is a string"
-mybool = True
-mylist = [0, 1, "two", 3.2, False]
-mytuple = (0, 1, 2)
-mydict = {"one" : 1, "two" : 2}
+myInt = 5
+myFloat = 13.2
+myStr = "This is a string"
+myBool = True
+myList = [0, 1, "two", 3.2, False]
+myTuple = (0, 1, 2)
+myDict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(myInt)
+# print(myFloat)
+# print(myStr)
+# print(myBool)
+# print(myList)
+# print(myTuple)
+# print(myDict)
 
 # re-declaring a variable works
-
+myInt = "abc"
 # to access a member of a sequence type, use []
+# print(myList[2])
+# print(myTuple[1])
 
-# use slices to get parts of a sequence
-
-# you can use slices to reverse a sequence
-
+# # use slices to get parts of a sequence
+# print(myList[1:5])
+# print(myList[1:5:2])
+# # you can use slices to reverse a sequence
+# print(myList[::-1])
 # dictionaries are accessed via keys
-
+# print(myDict["one"])
 # ERROR: variables of different types cannot be combined
+# print("string" + str(123))
 
 # Global vs. local variables in functions
+def someFunction():
+    global myStr
+    myStr = "def"
+    print(myStr)
+    
+someFunction()
+print(myStr)
 
+del myStr 
+print(myStr) # ERROR: myStr is not defined
